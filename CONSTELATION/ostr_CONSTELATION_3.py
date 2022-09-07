babaa45fb9d78f157654413a119fa28180548065
warrenau <warrenau@oregonstate.edu>
2022-09-07 14:11:24 -0700

    removed second star sim references
    removed anything having to do with STARBot

--- CONSTELATION/ostr_CONSTELATION_3.py
+++ CONSTELATION/ostr_CONSTELATION_3.py
@@ -1,10 +1,10 @@
 ############################################################
 #                                                           #
-#          STAR and Serpent Coupling Script v 0.5           #
+#          STAR and Serpent Coupling Script v 0.5.1         #
 #                   CONSTELLATION                           #
 #                                                           #
 # Created by: Cole Leingang                 2020/01/10      #
-#                                                           #
+# Modified by: Austin Warren                2022/09/07      #
 #############################################################
 
 import os
@@ -58,9 +58,7 @@ file_out.write('set comfile com.in com.out\n')
 file_out.write('\n')
 file_out.write('ifc HE3TOP.ifc\n\n')
 file_out.write('\n')
-file_out.write('ifc HE3BOT.ifc\n\n')
-file_out.write('\n')
-file_out.write('ifc fuel.ifc\n\n')
+file_out.write('ifc fuel.ifc\n\n')  # not sure that I will use the fuel ifc, but will leave in for now
 
 # Close new input file
 
@@ -124,59 +122,6 @@ else:
 
 file_out.close()
 
-##############################################
-# Write the initial He3 interface file for 2nd Star Run                         #
-# (He3 temperature and density for bottom two HENRI's will be updated)          #
-##############################################
-
-file_out = open('HE3BOT.ifc', 'w')
-
-# Write the header line (TYPE MAT OUT)
-
-file_out.write('2  He3Bot 0\n')
-
-# Write the mesh type
-
-file_out.write('1\n')
-
-# Write the mesh data (NX XMIN XMAX NY YMIN YMAX NZ ZMIN ZMAX)
-
-file_out.write('1 -100 100 1 -100 0 500 -60.48375 60.48375\n')
-
-# Write the Mesh Data
-STAR_Points = 501
-# Check to see if STAR-CCM_ .csv file has been created and wait for a predetermined amount of time if it hasn't
-time_to_wait = 10
-time_counter = 0
-filename = r'./ExtractedData/He3Data_table.csv'
-while not os.path.exists(filename):
-    time.sleep(1)
-    time_counter += 1
-    if time_counter > time_to_wait: break
-if os.path.isfile(filename):
-    # Take the data from STAR-CCM+ .csv file and write to ifc
-    columns = ['Position in Cartesian 1[X] (cm)', 'Density(g/cm^3) (kg/m^3)', 'Temperature (K)']
-    df = pd.read_csv(filename, usecols=columns)
-    # Position = df['Position in Cartesian 1[X] (cm)']
-    # Density =  df['Density(g/cm^3) (kg/m^3)']
-    # Temp = df['Temperature (K)']
-    
-    # Sorts data from STAR-CCM+ in ascending order by position
-    df_convert = df.to_numpy()
-    numpy_array = df_convert[df_convert[:, 0].argsort()]
-    
-    # Cross Section Data for He-3 in SERPENT2 only exists for 300 K or above, this will only happen early on in the transient and temp
-    # should never fall that far below 300K so setting this arbitrary constraint should have minimal change in results
-    for i in range(STAR_Points):
-        if numpy_array[i, 2] < 300.0:
-            numpy_array[i, 2] = 300.0
-    denstemp = numpy_array[:, [1, 2]]
-    np.savetxt(file_out, denstemp, fmt="%1.6f")
-else:
-    raise ValueError("%s has not been created or could not be read" % filename)
-# Close interface file
-
-file_out.close()
 
 ##############################################
 # Write the initial fuel interface           #
@@ -324,7 +269,7 @@ while simulating == 1:
         name = r'coupledTreat_det'+str(curtime)
     # Finds Heat Production Detector values from SERPENT2 Output
     HeadTop_pattern = re.compile(r"\DETSerpent2STop\s")
-    HeadBot_pattern = re.compile(r"\DETSerpent2SBot\s")
+
     # Finds Fuel Deposition Detector values from SERPENT2 Output
     Fuel_pattern = re.compile(r"\DETFuelDepositon\s")
     # Finds data after initial pattern has been found
@@ -343,7 +288,7 @@ while simulating == 1:
     Xpoints = 1
     # Finds Heat Production Detector Y values
     FourthTop_pattern = re.compile(r"DETSerpent2STopY(.*)$")
-    FourthBot_pattern = re.compile(r"DETSerpent2SBotY(.*)$")
+
     # Number of Y points
     Ypoints = 5
     # Number of overall points
@@ -374,10 +319,10 @@ while simulating == 1:
         for line in f1:
          matchx = Third_pattern.search(line)
          matchyTop = FourthTop_pattern.search(line)
-         matchyBot = FourthBot_pattern.search(line)
+
          matchz = Second_pattern.search(line)
          matchTop = HeadTop_pattern.match(line)
-         matchBot = HeadBot_pattern.match(line)
+
          matchfuel = Fuel_pattern.match(line)
          # Finds Volumetric Heating for Top 2 HENRIs
          if matchTop is not None:
@@ -441,37 +386,7 @@ while simulating == 1:
                      iter = iter +1
                      if iter == Fpoints:
                          break
-        # Finds Volumetric Heating for Bottom 2 HENRIs
-         if matchBot is not None:
-             iter = 0
-             for line in f1:
-                 matchpointsBot = data_pattern.search(line)
-                 if matchpointsBot is not None:
-                     savepointsBot = matchpointsBot.group(0)
-                     if iter < 10:
-                         # Save Data used to input x,y,z locations
-                         cleanpointsBot = savepointsBot[34:48]
-                         # Save Mean Value of Data
-                         cleanpassBot = savepointsBot[48:60]
-                         dataBot.append(cleanpointsBot)
-                         data_passBot.append(cleanpassBot)
-                     if iter < 999 and iter >= 10:
-                         # Save Data used to input x,y,z locations
-                         cleanpointsBot = savepointsBot[35:48]
-                         # Save Mean Value of Data
-                         cleanpassBot = savepointsBot[48:61]
-                         dataBot.append(cleanpointsBot)
-                         data_passBot.append(cleanpassBot)
-                     if iter >= 999: 
-                         # Save Data used to input x,y,z locations
-                         cleanpointsBot = savepointsBot[34:50]
-                         # Save Mean Value of Data
-                         cleanpassBot = savepointsBot[51:63]
-                         dataBot.append(cleanpointsBot)
-                         data_passBot.append(cleanpassBot)
-                     iter = iter +1
-                     if iter == points:
-                         break
+        
          if matchx is not None:
              iterx = 0
              for line in f1:
@@ -494,17 +409,7 @@ while simulating == 1:
                      itery = itery +1
                      if itery == Ypoints:
                          break
-         if matchyBot is not None:
-             itery = 0
-             for line in f1:
-                 matchyBot = data_pattern.search(line)
-                 if matchyBot is not None:
-                     saveyBot = matchyBot.group(0)
-                     cleanyBot= saveyBot[24:]
-                     YdataBot.append(cleanyBot)
-                     itery = itery +1
-                     if itery == Ypoints:
-                         break
+         
          if matchz is not None:
              iterz = 0
              for line in f1:
@@ -528,10 +433,10 @@ while simulating == 1:
     ny = list(range(1,Ypoints+1))
     nz = list(range(1,Zpoints+1))
     data_pass = [float(i) for i in data_pass]
-    data_passBot = [float(i) for i in data_passBot]
+
     Xdata = [float(i) for i in Xdata]
     Ydata = [float(i) for i in Ydata]
-    YdataBot = [float(i) for i in YdataBot]
+
     Zdata = [float(i) for i in Zdata]
     # saves fuel data
     data_fuelpass = [float(i) for i in data_fuelpass]
@@ -540,14 +445,14 @@ while simulating == 1:
      # Sets first and last set of Volumetric Heating to Zero, since this will define the Volumetric Heating Test Section
      if point <= Ypoints:
          data_pass[point] = data_pass[point]*0
-         data_passBot[point] = data_passBot[point]*0
+
      elif point >= points-Ypoints:
          data_pass[point] = data_pass[point]*0
-         data_passBot[point] = data_passBot[point]*0
+
      else:
      # Converts Mean Values of MeV/cm^3 to J/m^3-s to pass to STAR-CCM+ 
          data_pass[point] = (data_pass[point]*1E6)/timestep
-         data_passBot[point] = (data_passBot[point]*1E6)/timestep
+
     # Converts cm to m
     for xpoint in nx:
      # Note: Due to STAR-CCM+ being a 2-D simulation, X-Values (which would be Z Values) are set to zero
@@ -555,8 +460,7 @@ while simulating == 1:
     for ypoint in ny:
         # Y-Values stay the same in both codes (subtract SERPENT Distance from Origin to get STAR-CCM+ relative distance)
         Ydata[ypoint-1] = -1*(Ydata[ypoint-1]-40.605)/100
-        # Bottom data uses "negative" position values
-        YdataBot[ypoint-1] = -1*(YdataBot[ypoint-1]-40.605)/100
+
     for zpoint in nz:
      # Note: Due to orientation of STAR-CCM+ simulation Z Values are X Values in STAR
      Zdata[zpoint-1] = (Zdata[zpoint-1]/-100)+2.267903
@@ -589,44 +493,18 @@ while simulating == 1:
          # Writes to csv
          csv_writer.writerow(res)
     # Passes Bottom Data
-    with open(r'STAR_HeatBot.csv', 'wb') as f2:
-        # Sets up Title Headers for STAR-CCM+
-        Title = ['X(m)', 'Y(m)', 'Z(m)', 'VolumetricHeat(W/m^3)']
-        csv_writer = csv.writer(f2)
-        csv_writer.writerow(Title)
-    # Iterates through the number of points given in the SERPENT Output
-        for point in Np:
-         # Finds the integers used by SERPENT to show Z,Y,X Locations
-         temp = re.findall(r'\d+', data[point])
-         res = list(map(int,temp))
-         # Replaces X integer with actual location from SERPENT Output
-         for xpoint in nx:
-             if res[2] == xpoint:
-                 res[2] = Xdata[xpoint-1]
-         # Replaces Y integer with actual location from SERPENT Output
-         for ypoint in ny:
-             if res[1] == ypoint:
-                 res[1] = YdataBot[ypoint-1]
-         # Replaces Z Integer with actual location from SERPENT Output
-         for zpoint in nz:
-             if res[0] == zpoint:
-                 res[0] = Zdata[zpoint-1]
-         # Adds Mean Value for that location
-         res.append(data_passBot[point])
-         # Writes to csv
-         csv_writer.writerow(res)
-        time_counter = 0
+    
     if curtime == 0:
         ##############################################
         # Setup the STAR-CCM+ Simulation             #
         ###############################################
         # Simply submits STAR-CCM+ submission script to server
         run_STAR1 = "qsub STARTop_Job.sh"
-        run_STAR2 = "qsub STARBot_Job.sh"
+
         os.system(run_STAR1)
-        os.system(run_STAR2)
+
     # check to see if STAR is done executing
-    STARBot = r'./STARBotDone.txt'
+
     STARTop = r'./STARTopDone.txt'
     time_to_wait = 1000000
     time_counter = 0
@@ -634,10 +512,7 @@ while simulating == 1:
         time.sleep(1)
         time_counter += 1
         if time_counter > time_to_wait:break
-    while not os.path.exists(STARBot):
-        time.sleep(1)
-        time_counter += 1
-        if time_counter > time_to_wait:break
+
     if curtime > 0:
         # Write SERPENTDone.txt file indicating that the current loop has been completed and data extracted
         file_out = open('./SerpentDone.txt','w')
@@ -691,54 +566,7 @@ while simulating == 1:
     # Close interface file 
     
     file_out.close()
-    ###########################
-    # Update Bottom interface        #
-    ###########################
-    os.remove('HE3BOT.ifc')
-    file_out = open('HE3BOT.ifc', 'w')
     
-    file_out.write('2  He3Bot 0\n')
-    
-    # Write the mesh type
-    
-    file_out.write('1\n')
-    
-    # Write the mesh data (NX XMIN XMAX NY YMIN YMAX NZ ZMIN ZMAX)
-    
-    file_out.write('1 -100 100 1 0 -100 500 -60.48375 60.48375\n')
-    
-    # Check to see if STAR-CCM_ .csv file has been created and wait for a predetermined amount of time if it hasn't
-    time_to_wait = 1000000
-    time_counter = 0
-    filename = r'./ExtractedBotData/He3Data_table_'+str(STAR_STEP)+'.csv'
-    while not os.path.exists(filename):
-        time.sleep(1)
-        time_counter += 1
-        if time_counter > time_to_wait:break
-    if os.path.isfile(filename):
-        # Take the data from STAR-CCM+ .csv file and write to ifc
-        columns = ['Position in Cartesian 1[X] (cm)', 'Density(g/cm^3) (kg/m^3)', 'Temperature (K)']
-        df = pd.read_csv(filename, usecols=columns)
-        # Position = df['Position in Cartesian 1[X] (cm)']
-        # Density =  df['Density(g/cm^3) (kg/m^3)']
-        # Temp = df['Temperature (K)']
-        
-        # Sorts data from STAR-CCM+ in ascending order by position
-        df_convert = df.to_numpy()
-        numpy_array = df_convert[df_convert[:,0].argsort()]
-        
-        # Cross Section Data for He-3 in SERPENT2 only exists for 300 K or above, this will only happen early on in the transient and temp
-        # should never fall that far below 300K so setting this arbitrary constraint should have minimal change in results
-        for i in range(STAR_Points):
-            if numpy_array[i,2] < 300.0:
-                numpy_array[i,2] = 300.0
-        denstemp = numpy_array[:,[1,2]]
-        np.savetxt(file_out,denstemp, fmt = "%1.6f")
-    else:
-        raise ValueError("%s has not been created or could not be read" % filename)
-    # Close interface file 
-    
-    file_out.close()
     # Update STAR_STEP by number of steps that STAR takes before updating data
     STAR_STEP += step_length
 
@@ -819,16 +647,16 @@ while simulating == 1:
     # Archive Files                                      #####
     ##########################################################
     copyfile('HE3TOP.ifc','Archive/HE3TOP.ifc'+str(curtime))
-    copyfile('HE3BOT.ifc','Archive/HE3BOT.ifc'+str(curtime))
+
     copyfile('fuel.ifc', 'Archive/fuel.ifc' + str(curtime))
     copyfile('STAR_HeatTop.csv','Archive/Star_HeatTop'+str(curtime)+'.csv')
-    copyfile('STAR_HeatBot.csv','Archive/Star_HeatBot'+str(curtime)+'.csv')
+
     if curtime >= 2:
         copyfile('coupledTreat_res.m','Archive/coupledTreat_res'+str(curtime)+'.m')
     # Delete Files that are not needed between iterations
     time.sleep(60)
     os.remove(STARTop)
-    os.remove(STARBot)
+
     if curtime > 0:
         os.remove('SerpentDone.txt')
     os.remove(textfile)
